690c634e93cbec7d3f746b68171bcf2c7c287e4c
unknown <prerana.bhasein@gmail.com>
2021-11-20 20:21:59 +0530

    ui changes

--- Extension-secureway/background.js
+++ Extension-secureway/background.js
@@ -2,7 +2,4 @@ chrome.extension.onRequest.addListener(function(prediction){
     if (prediction == 1){
         alert("Warning: Phishing detected!!");
     }
-    else if (prediction == -1){
-        alert("No phishing detected");
-    }
 });
