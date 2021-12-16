5137cdeb8d8a7a63b0e5050ce9f06c3ad3d4b467
Sarthak Khabiya <46020024+Sarthak8989@users.noreply.github.com>
2021-12-16 19:34:23 +0530

    UI Final

--- Extension-secureway/popup.js
+++ Extension-secureway/popup.js
@@ -1,35 +1,33 @@
-// function activate(id, reload) {
-//     for (var i = 0; i < 5; i++) document.getElementById("l" + i).className = "";
-//     document.getElementById(id).className = "lactive";
+function save_options() {
+  var lowcheck = document.getElementById('Low').checked;
+  var mediumcheck = document.getElementById('Medium').checked;
+  chrome.storage.sync.set({
+    low: lowcheck,
+    medium: mediumcheck
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
 
-//     if (reload) {
-//         chrome.runtime.sendMessage({type: "level", level: parseInt(id[1])}, function (resp) {
-//             console.log(resp);
-//         });
-//     }
-
-//     chrome.storage.sync.set({'level': parseInt(id[1])}, function () {
-//     });
-
-// }
-
-// document.getElementById("l0").addEventListener("click", function () {
-//     activate("l0", true);
-// });
-// document.getElementById("l1").addEventListener("click", function () {
-//     activate("l1", true);
-// });
-// document.getElementById("l2").addEventListener("click", function () {
-//     activate("l2", true);
-// });
-// document.getElementById("l3").addEventListener("click", function () {
-//     activate("l3", true);
-// });
-// document.getElementById("l4").addEventListener("click", function () {
-//     activate("l4", true);
-// });
-
-// chrome.storage.sync.get("level", function (val) {
-//     activate("l" + val.level, false);
-// });
-
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    low: true,
+    medium: true
+  }, function(items) {
+    document.getElementById('Low').checked = items.low;
+    document.getElementById('Medium').checked = items.medium;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+el = document.getElementById('save')
+if(el){
+  el.addEventListener('click',save_options);
+}
