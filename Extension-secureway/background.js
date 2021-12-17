2d13d6c244c95695876aa212820b35846dd9d580
unknown <prerana.bhasein@gmail.com>
2021-12-18 01:39:38 +0530

    functionalities completed

--- Extension-secureway/background.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onRequest.addListener(function(prediction){
-    if (prediction == 1){
-        alert("Warning: Phishing detected!!");
-        return 1;
-    }
-});
-
--- Extension-secureway/popup.js
+++ Extension-secureway/popup.js
@@ -1,12 +1,10 @@
 function save_options() {
   var lowcheck = document.getElementById('Low').checked;
-  var mediumcheck = document.getElementById('Medium').checked;
   chrome.storage.sync.set({
     low: lowcheck,
-    medium: mediumcheck
   }, function() {
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
+    var status = document.getElementById('Low');
     status.textContent = 'Options saved.';
     setTimeout(function() {
       status.textContent = '';
@@ -17,17 +15,18 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     low: true,
-    medium: true
   }, function(items) {
     document.getElementById('Low').checked = items.low;
-    document.getElementById('Medium').checked = items.medium;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-el = document.getElementById('save')
-if(el){
-  el.addEventListener('click',save_options);
-}
+
+document.querySelector('#go-to-options').addEventListener('click', function() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('urlinfo.html'));
+  }
+});
